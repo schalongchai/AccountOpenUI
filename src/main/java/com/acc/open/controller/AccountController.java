31dc481156baa2c0f8bdc6200167f3936eb87179
e1045469 <e1045469@APLT1045469N.FNFIS.com>
2019-05-15 15:57:19 +0700

    Added account info dialog

--- src/main/java/com/acc/open/controller/AccountController.java
+++ src/main/java/com/acc/open/controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.acc.open.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.acc.open.model.AoAccountInfo;
+import com.acc.open.service.AccountService;
 import com.acc.open.service.LogingService;
 
 @Controller
@@ -15,10 +20,27 @@ public class AccountController {
 	@Autowired
 	LogingService loginService;
 	
+	@Autowired
+	private AccountService cs;
+	
 	@RequestMapping(value = "/account", method = RequestMethod.GET)
 	public String account(ModelMap model,HttpServletRequest request) {
 		model.addAttribute("tabActive","Account");
 		return "bulkaccount/list";
 		
+	}
+	
+	@RequestMapping(value = "/accountinfo", method = RequestMethod.GET)
+	public String getAccountInfo(@RequestParam("cif") String cif, ModelMap model) {
+		String ret="";
+		List<AoAccountInfo> accounts = cs.getAccountById(cif);
+		if(accounts!=null){
+			model.addAttribute("accounts", accounts);
+			ret = "account/info";
+		}else {
+			model.addAttribute("response", "Account not found!");
+			ret = "error/error";
+		}
+		return ret;
 	}
 }
--- src/main/java/com/acc/open/service/CustomerService.java
+++ src/main/java/com/acc/open/service/CustomerService.java
@@ -9,6 +9,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.acc.open.model.AoCustomerInfo;
@@ -58,8 +59,8 @@ public class CustomerService {
 	    try {
 	    	ResponseEntity<List<AoCustomerInfo>> response = restTemplate.exchange(uri, HttpMethod.GET,null, new ParameterizedTypeReference<List<AoCustomerInfo>>(){},params);
 	    	customer = response.getBody();
-	    }catch(Exception e) {
-	    	
+	    } catch (HttpClientErrorException e) {
+
 	    }
 	    return customer;
 	}
