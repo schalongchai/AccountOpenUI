f9be5579f476bfef59388a3740df22e16799404f
e1045469 <e1045469@APLT1045469N.FNFIS.com>
2019-05-27 18:04:37 +0700

    Added function to upload file

--- src/main/java/com/acc/open/controller/BulkController.java
+++ src/main/java/com/acc/open/controller/BulkController.java
@@ -2,13 +2,14 @@ package com.acc.open.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.acc.open.model.AoBulkFile;
 import com.acc.open.service.BulkService;
 import com.acc.open.service.LogingService;
@@ -22,8 +23,11 @@ public class BulkController {
 	private BulkService bs;
 	
 	@RequestMapping(value = "/bulkupload", method = RequestMethod.POST)
-	public String account(ModelMap model,HttpServletRequest request) {
-	
+	public String account(ModelMap model,@RequestParam("bulkfile") MultipartFile file) {
+		AoBulkFile a = bs.readFile(file);
+		if(a!=null) {
+		 bs.addBulkFile(a);
+		}
 		List<AoBulkFile> bo = bs.getAllBulks();
 		model.addAttribute("tabActive","Account");
 		model.addAttribute("bulkfiles", bo);
--- src/main/java/com/acc/open/service/BulkService.java
+++ src/main/java/com/acc/open/service/BulkService.java
@@ -8,8 +8,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.acc.open.model.AoBulkFile;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class BulkService {
@@ -33,22 +35,37 @@ public class BulkService {
 	    return bulkFile;
 	}
 	
-	public List<AoBulkFile> addBulkFile()
+	
+	public void addBulkFile(AoBulkFile bulkfile)
 	{
-		List<AoBulkFile> bulkFile = null;
 		final String uri = restURI + "/api/bulkfiles/";
 		try {
 		    RestTemplate restTemplate = new RestTemplate();
-		    ResponseEntity<List<AoBulkFile>> response = restTemplate.exchange(uri, HttpMethod.POST,null, new ParameterizedTypeReference<List<AoBulkFile>>(){});
-		    bulkFile = response.getBody();
+			ResponseEntity<AoBulkFile> response = restTemplate.postForEntity(uri, bulkfile, AoBulkFile.class);
+			response.getBody();
 		} catch (HttpClientErrorException e) {
 			
 		}
 		
 
-	    return bulkFile;
-	}
 
-		
+	}
 	
+	
+	public AoBulkFile readFile(MultipartFile file) {
+		 AoBulkFile a = null;
+		 if (!file.isEmpty()) {
+		        try {
+		            byte[] bytes = file.getBytes();
+		            String data = new String(bytes);
+		            a = new  ObjectMapper().readValue(data, AoBulkFile.class);
+		        }
+		        catch (Exception e) {
+		        	
+		        }
+
+		 }
+		 return a;
+	
+	}
 }
