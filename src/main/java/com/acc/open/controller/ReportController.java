6c6c354dd9cb5a18dcb0d7b26a366b2047aa9454
e1045469 <e1045469@APLT1045469N.FNFIS.com>
2019-05-31 16:31:55 +0700

    Changed report output to populate file download

--- src/main/java/com/acc/open/controller/ReportController.java
+++ src/main/java/com/acc/open/controller/ReportController.java
@@ -1,9 +1,13 @@
 package com.acc.open.controller;
 
+import java.io.ByteArrayInputStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,35 +16,30 @@ import com.acc.open.service.ReportService;
 
 @Controller
 public class ReportController {
-	
+
 	@Autowired
 	ReportService rs;
-	
-	@RequestMapping(value = "/download", method = RequestMethod.GET)
-	public String reportDownload(ModelMap model,
-			@RequestParam(name="sdate") String sdate,
-			@RequestParam(name="edate") String edate,
-			@RequestParam(name="branch") String branch) {
 
-		final String link = rs.getReport(sdate, edate, branch);
-		
-		if(!link.equals("")) {
-			model.addAttribute("downloadLink",link);
-		}
-		model.addAttribute("sdate",sdate);
-		model.addAttribute("edate",edate);
-		model.addAttribute("branch",branch);
-		model.addAttribute("tabActive","Report");
-		
-		return "report/list";
+	@RequestMapping(value = "/download", method = RequestMethod.GET)
+	public ResponseEntity<InputStreamResource> reportDownload(ModelMap model,
+			@RequestParam(name = "sdate") String sdate, 
+			@RequestParam(name = "edate") String edate,
+			@RequestParam(name = "branch") String branch) {
+
+		byte[] data = rs.downloadFile(sdate, edate, branch);
+		ByteArrayInputStream in = new ByteArrayInputStream(data);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Disposition", "attachment; filename=accounts.xlsx");
+		headers.add("Content-Type", "application/vnd.ms-excel; charset=utf-8");
+		return ResponseEntity.ok().headers(headers).body(new InputStreamResource(in));
+
 	}
-	
+
 	@RequestMapping(value = "/report", method = RequestMethod.GET)
 	public String report(ModelMap model) {
-	    model.addAttribute("tabActive","Report");
+		model.addAttribute("tabActive", "Report");
 		return "report/list";
 	}
-	
-	
-	
+
 }
--- src/main/java/com/acc/open/service/ReportService.java
+++ src/main/java/com/acc/open/service/ReportService.java
@@ -1,23 +1,38 @@
 package com.acc.open.service;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ReportService {
-    @Value("${spring.datasource.url}")
-    private String restURI;
-    
-	
-	public String getReport(String sdate,String edate,String branch) {
-		
-		if(branch.equals("")) {
-			branch="all";
+	@Value("${spring.datasource.url}")
+	private String restURI;
+
+	public byte[] downloadFile(String sdate, String edate, String branch) {
+
+		if (branch.equals("")) {
+			branch = "all";
 		}
-		
-		final String uri = restURI + "/api/report/account.rpt?sdate="+sdate+"&edate="+edate+"&branch="+branch;
-		return uri;
-		
-	   
+
+		final String uri = restURI + "/api/report/account.rpt?sdate=" + sdate + "&edate=" + edate + "&branch=" + branch;
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getMessageConverters().add(new ByteArrayHttpMessageConverter());
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_OCTET_STREAM));
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+		ResponseEntity<byte[]> response = restTemplate.exchange(uri, HttpMethod.GET, entity, byte[].class, "1");
+		return response.getBody();
+
 	}
+
 }
